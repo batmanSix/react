d6349cdd701121ecf02bd674c5e6e5ced2dd4c00
kangbo <kangbo@vchangyi.com>
2019-11-04 15:49:44 +0800

    修改文件

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState , useEffect } from 'react';
+function Example(){
+    const [ count , setCount ] = useState(0);
+    //---关键代码---------start-------
+    useEffect(()=>{
+        console.log(`useEffect=>You clicked ${count} times`)
+    })
+    //---关键代码---------end-------
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <p>You clicked {count} times</p>
+            <button onClick={()=>{setCount(count+1)}}>click me</button>
+        </div>
+    )
 }
+export default Example;
 
-export default App;
